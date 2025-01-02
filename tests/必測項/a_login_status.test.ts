676ad93ad616de20113d725f890643dc97407747
黃珮慈 <cency@huangpeicide-MacBook-Air.local>
2025-01-02 19:09:50 +0800

    feat:test

--- tests/必測項/a_login_status.test.ts
+++ tests/必測項/a_login_status.test.ts
@@ -5,7 +5,7 @@ test('一般登入', async ({ page }) => {
   await page.getByRole('textbox', { name: '請輸入身分證字號' }).fill('B222790712');
   
   //await page.getByPlaceholder('請輸入密碼').fill('2222bright'); 
-  await page.locator('#password').fill('1111bright');
+  await page.locator('#password').fill('2222bright');
   await page.locator('#password').press('Tab');
   await page.getByRole('textbox', { name: '請輸入驗證碼' }).click();
   await page.waitForTimeout(10000); // 暫停 輸入驗證碼
