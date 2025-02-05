bb8ad69d428633d257b45e5a648ed1833790c45e
黃珮慈 <cency@huangpeicide-MacBook-Air.local>
2025-02-05 09:36:37 +0800

    feat UAT login

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,8 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
+dotenv.config();
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
@@ -18,7 +19,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -26,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'https://www.pocket.tw',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -34,6 +35,30 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+
+    {
+      //測試環境
+      name:'prod',
+      use: {
+        baseURL: 'https://www.pocket.tw',
+/*         env:{
+          USERNAME: process.env.userID,
+          PASSWORD: process.env.password,
+        } */
+      },
+    },
+
+    {
+      //正式環境
+      name:'uat',
+      use: {
+        baseURL: 'https://www.labpocket.tw',
+/*         env:{
+          USERNAME: process.env.uat_userID,
+          PASSWORD: process.env.uat_password,
+        } */
+      },
+    },
 
     {
       //Setup project
@@ -47,17 +72,17 @@ export default defineConfig({
       dependencies: ['setup db'],
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox']},
-      dependencies: ['setup db'],
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox']},
+    //   dependencies: ['setup db'],
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari']},
-      dependencies: ['setup db'],
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari']},
+    //   dependencies: ['setup db'],
+    // },
 
     /* Test against mobile viewports. */
     {
@@ -70,7 +95,6 @@ export default defineConfig({
        use: { ...devices['iPhone 12']},
        dependencies: ['setup db'],
     },
-
 
     /* Test against branded browsers. */
     // {
@@ -88,5 +112,5 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-  ]
+]
 });
