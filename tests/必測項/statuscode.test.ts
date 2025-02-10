5af0596e451684a8b16fd423877f3fde3486d4fd
黃珮慈 <cency@huangpeicide-MacBook-Air.local>
2025-02-10 19:20:21 +0800

    整理檔案

--- tests/必測項/statuscode.test.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  const response = await page.goto('https://www.pocket.tw/');
-  expect (response.status()).toBe(200);
-});
