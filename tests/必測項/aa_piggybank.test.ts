9aae29c1e59a9ee05d0e5cc2d076db79e2265ac2
黃珮慈 <cency@huangpeicide-MacBook-Air.local>
2025-02-11 01:30:14 +0800

    [fix]憑證狀態修改為使用函數

--- tests/必測項/aa_piggybank.test.ts
+++ tests/必測項/aa_piggybank.test.ts
@@ -1,15 +1,13 @@
 import { test, expect } from '@playwright/test';
-
-test.use({ storageState:'auth.json'}); //使用登入狀態及憑證
-test.use({ storageState:'webca.json'});
+import { webca } from "../../utils/webca.spec"
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.pocket.tw/piggybank/');
+  await webca(page);
+  await page.goto('/piggybank/');
   await page.locator('iframe[name="piggy_bank_AMT"]').contentFrame().getByRole('textbox', { name: '請輸入標的名稱或代號' }).click();
   await page.locator('iframe[name="piggy_bank_AMT"]').contentFrame().getByRole('textbox', { name: '請輸入標的名稱或代號' }).fill('0050');
   await page.locator('iframe[name="piggy_bank_AMT"]').contentFrame().getByText('元大台灣50').click();
   await page.locator('iframe[name="piggy_bank_AMT"]').contentFrame().getByRole('button', { name: '定股' }).click();
-  await page.locator('iframe[name="piggy_bank_AMT"]').contentFrame().getByRole('textbox', { name: '1~' }).click();
   await page.locator('iframe[name="piggy_bank_AMT"]').contentFrame().getByRole('textbox', { name: '1~' }).fill('1');
   await page.locator('iframe[name="piggy_bank_AMT"]').contentFrame().getByRole('button', { name: '+新增委託日期' }).click();
   await page.locator('iframe[name="piggy_bank_AMT"]').contentFrame().getByRole('button', { name: '2', exact: true }).click();
@@ -18,6 +16,3 @@ test('test', async ({ page }) => {
   await page.locator('iframe[name="piggy_bank_AMT"]').contentFrame().getByRole('button', { name: '確認' }).click();
 
 });
-
-// test
-//12345
