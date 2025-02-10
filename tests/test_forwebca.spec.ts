8b57210234f347a0d9215c0ccdbe220c74e259e1
黃珮慈 <cency@huangpeicide-MacBook-Air.local>
2025-02-11 00:57:29 +0800

    新增申請憑證函式及測試用檔

---
+++ tests/test_forwebca.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+import { webca } from '../utils/webca.spec' //使用憑證狀態
+test.use({ storageState:'webca.json'});
+
+test('withdraw', async ({ page }) => {
+  await webca(page);
+  await page.goto('/wallet/');
+  await page.getByRole('link', { name: '出款申請' }).click();
+  await page.getByPlaceholder('請輸入出款金額').fill('5');
+  await page.getByPlaceholder('請輸入驗證碼').click();
+  await page.waitForTimeout(10000);
+  await page.getByRole('button', { name: '送出' }).click();
+  await page.getByRole('button', { name: '確認' }).click();
+  await expect(page.getByText('出款申請成功')).toBeVisible();
+});
