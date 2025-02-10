665b18c5413bb4f15de292b8a4a9850b9f7a075a
黃珮慈 <cency@huangpeicide-MacBook-Air.local>
2025-02-11 00:06:42 +0800

    加上判斷登入狀態

--- utils/login.spec.ts
+++ utils/login.spec.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import { Page, test } from '@playwright/test';
-import { constants } from 'buffer';
-// import test from 'node:test';
+
+test.use({ storageState:'auth.json'});
 
 dotenv.config();
 /* const USERNAME = process.env.userID || ""
@@ -26,21 +26,35 @@ export async function login(page: Page) {
 
   // 前往登入頁面
   await page.goto('');
-  // 填寫帳號
-  await page.getByRole('textbox', { name: '請輸入身分證字號' }).fill(USERNAME);
 
-  // 填寫密碼
-  await page.locator('#password').fill(PASSWORD);
-  await page.locator('#password').press('Tab');
+  // 檢查是否顯示登入提示文字
+  const loginPrompt = await page.locator('#homepagebg').getByText('登入後提供更多服務').count();
+    
+  if (loginPrompt > 0) {
+    console.log('需登入');
+    // 填寫帳號
+    await page.getByRole('textbox', { name: '請輸入身分證字號' }).fill(USERNAME);
 
-  // 填寫驗證碼（手動輸入驗證碼）
-  await page.getByRole('textbox', { name: '請輸入驗證碼' }).click();
-  await page.waitForTimeout(10000); // 等待使用者輸入驗證碼
+    // 填寫密碼
+    await page.locator('#password').fill(PASSWORD);
+    await page.locator('#password').press('Tab');
+  
+    // 填寫驗證碼（手動輸入驗證碼）
+    await page.getByRole('textbox', { name: '請輸入驗證碼' }).click();
+    await page.waitForTimeout(10000); // 等待使用者輸入驗證碼
+  
+    // 點擊登入按鈕
+    await page.getByRole('button', { name: '登入' }).click();
+  
+    // 等待登入完成並儲存狀態
+    await page.waitForTimeout(5000);
+     // 保存登入狀態到檔案
+    await page.context().storageState({ path: 'auth.json' });
+  } else{
+    console.log('不需登入');
+  }
 
-  // 點擊登入按鈕
-  await page.getByRole('button', { name: '登入' }).click();
+  
 
-  // 等待登入完成並儲存狀態
-  await page.waitForTimeout(5000);
-  await page.context().storageState({ path: 'auth.json' }); // 保存登入狀態到檔案
+
 }
