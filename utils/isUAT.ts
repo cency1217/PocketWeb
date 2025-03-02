3579685d8deecef07ae7af19582bac37d2f9af82
黃珮慈 <cency@huangpeicide-MacBook-Air.local>
2025-03-03 02:36:22 +0800

    [feat]判斷環境函數更新

--- utils/isUAT.ts
+++ utils/isUAT.ts
@@ -1,21 +1,19 @@
 import dotenv from 'dotenv';
 import { Page, test } from '@playwright/test';
-
 dotenv.config();
 
 export async function isUAT(page: Page) {
-  //
   test.slow()
-
+    // 根據 baseURL 判斷環境
   const baseURL = test.info().project.use.baseURL || '';
-  // 根據 baseURL 判斷環境
   const isUAT = baseURL.includes('labpocket');
   const ENV = isUAT ? 'uat' : 'prod';
   console.log(`當前環境: ${ENV}`);
 
-  // 根據環境設定帳號、密碼
+  // 根據環境設定帳號、密碼、生日
   const USERNAME = isUAT ? process.env.uat_userID || '' : process.env.userID || '';
   const PASSWORD = isUAT ? process.env.uat_password || '' : process.env.password || '';
+  const BIRTHDAY = isUAT ? process.env.uat_bday || '' : process.env.bday || '';
   // 傳回 USERNAME 和 PASSWORD
-  return { USERNAME, PASSWORD };
+  return { USERNAME, PASSWORD, BIRTHDAY };
 }
