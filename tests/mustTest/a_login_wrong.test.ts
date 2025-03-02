6e043a35caa6cee26c586fa1ebb16e9ad051fb97
黃珮慈 <cency@huangpeicide-MacBook-Air.local>
2025-03-03 00:20:50 +0800

    [feat]必測項 登入失敗 整理

--- tests/mustTest/a_login_wrong.test.ts
+++ tests/mustTest/a_login_wrong.test.ts
@@ -1,30 +1,38 @@
 import { test, expect } from '@playwright/test';
+import { isUAT } from '../../utils/isUAT'
 
-test('帳號/密碼/驗證碼輸入錯誤提示', async ({ page }) => {
+test('pwd_wrong_hint', async ({ page }) => {
   test.slow();
-  await page.goto('https://www.pocket.tw/');
-  await page.getByRole('textbox', { name: '請輸入身分證字號' }).fill('B222790712');
-  await page.locator('#password').press('Tab');
+  const {USERNAME} = await isUAT(page);
+  await page.goto('');
+  await page.getByRole('textbox', { name: '請輸入身分證字號' }).fill(USERNAME);
   await page.locator('#password').fill('a12345678');
-  await expect(page.locator('#password')).toHaveAttribute('type','password');
   await page.getByRole('textbox', { name: '請輸入驗證碼' }).click();
-  await page.waitForTimeout(10000); //暫停輸入驗證碼
+  await page.waitForTimeout(7000); //暫停輸入驗證碼
   await page.getByRole('button', { name: '登入' }).click();
   await expect(page.getByText('* 帳號或密碼有誤，請再次確認')).toBeVisible();
-  await page.getByRole('textbox', { name: '請輸入身分證字號' }).fill('B222790713');
-  await page.locator('#password').fill('1111bright');
+});
+
+test('id_wrong_hint', async ({ page }) => {
+  test.slow();
+  await page.goto('');
+  await page.getByRole('textbox', { name: '請輸入身分證字號' }).fill('A000000000');
+  await page.locator('#password').fill('z12345678');
   await page.getByRole('textbox', { name: '請輸入驗證碼' }).clear();
   await page.getByRole('textbox', { name: '請輸入驗證碼' }).click();
-  await page.waitForTimeout(10000); //暫停輸入驗證碼
+  await page.waitForTimeout(7000); //暫停輸入驗證碼
   await page.getByRole('button', { name: '登入' }).click();
   await expect(page.getByText('* 帳號或密碼有誤，請再次確認')).toBeVisible();
-  await page.getByRole('textbox', { name: '請輸入身分證字號' }).fill('B222790712');
-  await page.locator('#password').press('Tab');
-  await page.locator('#password').fill('1111bright');
+});
+
+test('captcha_wrong_hint', async ({ page }) => {
+  test.slow();
+  const { USERNAME , PASSWORD } = await isUAT(page);
+  await page.goto('');
+  await page.getByRole('textbox', { name: '請輸入身分證字號' }).fill(USERNAME);
+  await page.locator('#password').fill(PASSWORD);
   await page.getByRole('textbox', { name: '請輸入驗證碼' }).clear();
   await page.getByRole('textbox', { name: '請輸入驗證碼' }).fill('11111'); //輸入錯誤驗證碼
   await page.getByRole('button', { name: '登入' }).click();
   await expect(page.getByText('* 驗證碼輸入錯誤')).toBeVisible();
-  await page.locator('#homepagebg path').first().click();
-  await expect(page.locator('#password')).toHaveAttribute('type','text');
 });
