d5c846a94cb3bf680564053842f3b6136687d32f
黃珮慈 <cency@huangpeicide-MacBook-Air.local>
2025-03-08 22:47:07 +0800

    [feat]驗證碼函數更新

--- utils/getCaptcha.ts
+++ utils/getCaptcha.ts
@@ -4,19 +4,19 @@ import Tesseract from 'tesseract.js';
 export async function getCaptcha(page: Page) {
   test.slow();
   // 1. 前往需要驗證碼的網頁
-  await page.goto('');
+  await page.goto('' , {waitUntil: 'domcontentloaded'});
 
   // 2. 擷取驗證碼圖片
   const captchaImage = await page.getByRole('img',{name:'captchaImage'});
-  await captchaImage.screenshot({ path: 'captcha.png' });
+  await captchaImage.screenshot({ path: 'test-files/captcha.png' });
 
  // 3. 使用 Tesseract.js 進行 OCR 辨識
-  const { data: { text } } = await Tesseract.recognize('captcha.png', 'eng', {
+  const { data: { text } } = await Tesseract.recognize('test-files/captcha.png', 'eng', {
         //logger: m => console.log(m) // 可用來監測 OCR 進度
   });
 
   // 過濾掉非數字與大寫字母
-  const captchaCode = text.replace(/[^A-Z0-9]/g, '').trim();
+  const captchaCode = text.replace(/\D/g, '').trim().substring(0, 5);
 
   console.log('OCR 識別出的驗證碼:', captchaCode);
   return { captchaCode };
