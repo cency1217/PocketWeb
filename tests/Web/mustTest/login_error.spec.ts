c1b7cbfe4096f1dafd07539c517e4e80a57ffe90
ceneyhuang <cencyhuang@gmail.com>
2025-03-20 16:32:17 +0800

    [ref]驗證碼加入captcha

--- tests/Web/mustTest/login_error.spec.ts
+++ tests/Web/mustTest/login_error.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { getUser } from '@utils/auth/getUser'
 import { genPwd } from '@utils/helper/genPwd'
+import { getCaptcha } from '@utils/captcha/getCaptcha'
 
 test('pwd_wrong_hint', async ({ page }) => {
   test.slow();
@@ -12,9 +13,10 @@ test('pwd_wrong_hint', async ({ page }) => {
   await page.locator('#password').fill(genPwd());
   await page.getByRole('textbox', { name: '請輸入驗證碼' }).click();
 
-  //暫停輸入驗證碼
-  await page.waitForTimeout(7000); 
-  await page.getByRole('button', { name: '登入' }).click();
+  const ocrSuccess = await getCaptcha(page);
+  if (!ocrSuccess) {
+      throw new Error('驗證碼辨識失敗');
+  };
 
   //驗證
   await expect(page.getByText('* 帳號或密碼有誤，請再次確認')).toBeVisible();
@@ -28,11 +30,11 @@ test('id_wrong_hint', async ({ page }) => {
   await page.getByRole('textbox', { name: '請輸入身分證字號' }).fill('A000000000');
   await page.locator('#password').fill('z12345678');
   await page.getByRole('textbox', { name: '請輸入驗證碼' }).clear();
-  await page.getByRole('textbox', { name: '請輸入驗證碼' }).click();
 
-  //暫停輸入驗證碼
-  await page.waitForTimeout(7000); 
-  await page.getByRole('button', { name: '登入' }).click();
+  const ocrSuccess = await getCaptcha(page);
+  if (!ocrSuccess) {
+      throw new Error('驗證碼辨識失敗');
+  };
 
   //驗證
   await expect(page.getByText('* 帳號或密碼有誤，請再次確認')).toBeVisible();
